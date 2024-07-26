a06b3690d10f332caae547bb2dac0425b0194d9a
Swadesh Behera <itswadesh@gmail.com>
2024-07-26 11:44:49 +0530

    packae upgrade

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+  import { toggleMode } from 'mode-watcher'
+  import Sun from 'lucide-svelte/icons/sun'
+  import Moon from 'lucide-svelte/icons/moon'
+  import { Button } from '$lib/components/ui/button'
+</script>
+
 <footer
   class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
@@ -59,3 +66,13 @@
     </p>
   </div>
 </footer>
+
+<Button on:click={toggleMode} variant="outline" size="icon">
+  <Sun
+    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+  />
+  <Moon
+    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+  />
+  <span class="sr-only">Toggle theme</span>
+</Button>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
+
   kit: {
     adapter: adapter()
   },
-  preprocess: vitePreprocess()
-};
-export default config;
+  vitePlugin: {
+    inspector: {
+      toggleKeyCombo: 'meta-shift',
+      showToggleButton: 'always',
+      toggleButtonPos: 'bottom-right'
+    }
+  }
+}
+
+export default config
